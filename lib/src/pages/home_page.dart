9ae01d79453b643972e3689c1b9220be7f51881f
viqui alaro <viqui.alaro@impuestos.gob.bo>
2020-11-13 08:58:38 -0400

    cambios

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -23,7 +23,7 @@ class HomePage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          print('Hola Viqui!');
+          print('Hola Viqui Alaro!');
         },
       ),
     );
